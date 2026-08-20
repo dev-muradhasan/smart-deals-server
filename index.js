15bdcf31c818889388acb8c55ffcc7c7314ac707
Murad Hasan <mdmuradhasan151@gmail.com>
2026-08-20 13:15:20 +0600

    save user data to database successfuly

--- index.js
+++ index.js
@@ -29,6 +29,21 @@ async function run() {
         const db = client.db("smart_db");
         const productsCollection = db.collection('products');
         const bidsCollection = db.collection('bids');
+        const usersCollection = db.collection('users')
+
+        app.post('/users', async(req, res)=>{
+            const newUser = req.body;
+            const email = req.body.email;
+            const query = {email: email};
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser){
+                res.send({message:'user already exist'})
+            }
+            else{
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            }
+        })
 
         app.get('/products', async(req, res)=>{
             // const projectFields = {title: 1}
